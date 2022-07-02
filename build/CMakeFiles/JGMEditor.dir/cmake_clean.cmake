c12de0581ec6ff3643e2efd989d048abd4b6f598
JGM01 <jacobgermanamccray@gmail.com>
2022-07-02 18:08:13 -0400

    It begins

---
+++ build/CMakeFiles/JGMEditor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JGMEditor.dir/main.cpp.o"
+  "JGMEditor"
+  "JGMEditor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JGMEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
